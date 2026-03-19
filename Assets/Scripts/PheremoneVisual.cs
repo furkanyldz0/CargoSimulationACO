e6f544f61519c458a18fbb3572332e93a66b8d1b
Furkan Yıldız <furkanyld0@hotmail.com>
2026-03-19 23:50:42 +0300

    added pheremone visualization on edges

---
+++ Assets/Scripts/PheremoneVisual.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class PheremoneVisual : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer roadMeshRenderer;
+    private Road road;
+    private MaterialPropertyBlock propertyBlock;
+    private Color color;
+    private float pheremoneMaxAlphaValue = 50f;
+
+    private void Start() {
+        road = GetComponent<Road>();
+        color = roadMeshRenderer.sharedMaterial.color;
+        Debug.Log(color.a);
+
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update() {
+
+        color.a = Math.Clamp(road.pheromoneLevel / pheremoneMaxAlphaValue, 0f, 1f);
+
+        propertyBlock.SetColor("_BaseColor", color);
+        roadMeshRenderer.SetPropertyBlock(propertyBlock);
+    }
+}
