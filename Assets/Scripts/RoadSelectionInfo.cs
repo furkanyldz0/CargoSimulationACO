5cb99f256158f163c6b1c7a2125bcd820b18d595
Furkan Yıldız <furkanyld0@hotmail.com>
2026-05-21 19:26:06 +0300

    added displaying road distance info during simulation

---
+++ Assets/Scripts/RoadSelectionInfo.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+
+public class RoadSelectionInfo : MonoBehaviour
+{
+    [SerializeField] private LayerMask roadLayer = new LayerMask();
+    [SerializeField] private TextMeshProUGUI roadInfoText;
+
+    private int raycastFreqPerSecond = 60;
+    private float checkRaycastTime;
+    private float checkRaycastTimeDelta;
+
+    private void Start() {
+        checkRaycastTime = 1f / (float) raycastFreqPerSecond;
+    }
+
+    private void Update()
+    {
+        if (!LevelManager.Instance.IsSimulationInitiated)
+            return;
+
+
+        if (checkRaycastTimeDelta > 0f) {
+            checkRaycastTimeDelta -= Time.deltaTime; 
+        }
+
+        if(checkRaycastTimeDelta <= 0f) {
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 10000f, roadLayer)) {
+                Road highlightedRoad = raycastHit.transform.GetComponentInParent<Road>();
+                if (highlightedRoad != null) {
+                    string startCityName = highlightedRoad.startCitySO.name;
+                    string endCityName= highlightedRoad.endCitySO.name;
+
+                    roadInfoText.SetText(($"{startCityName} - {endCityName} Yolu" +
+                        $"\r\n{highlightedRoad.distance.ToString("F2")} km"));
+                    roadInfoText.transform.position = mousePosition;
+                    roadInfoText.gameObject.SetActive(true);
+                }
+            }
+            else { //raycast ile herhangi bir yol nesnesi algýlanmadýðý zaman
+                roadInfoText.gameObject.SetActive(false);
+            }
+
+            checkRaycastTimeDelta = checkRaycastTime;
+        }
+
+        
+    }
+}
